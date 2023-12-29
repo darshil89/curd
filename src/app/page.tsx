bd7be3ec6d58b1aafc761dbfbc78a8f9babac512
Darshil Mahraur <darshilmahraur67@gmail.com>
2023-12-29 19:01:31 +0530

    rev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
   return (
     <div className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
       <PostCard />
-      
     </div>
   );
 }
